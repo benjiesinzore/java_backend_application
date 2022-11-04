f7c1b01c4e554f7cd90e0a278e86c860b99b778a
benjiesinzore <benjaminsinzore@gmail.com>
2022-11-04 12:37:21 +0300

    Handled Logging on console and also file.

--- src/main/java/com/javaAssignment/models/requestbody/security/CustomerReg.java
+++ src/main/java/com/javaAssignment/models/requestbody/security/CustomerReg.java
@@ -9,17 +9,22 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 @Data
 public class CustomerReg {
 
+    @NotBlank(message = "User ID is required.")
     int userID;
     @NotBlank(message = "User name is required.")
     String userName;
+    @NotBlank(message = "User email address is required.")
     String userEmailAddress;
+    @NotBlank(message = "User password is required.")
     String userPassword;
+    @NotBlank(message = "User confirmation password is required.")
     String confirmPassword;
 
     public CustomerReg(int userID, String userName, String userEmailAddress, String userPassword, String confirmPassword) {
